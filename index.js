6a3224f71a7eadb5dc42cdc24cb673ab75dc642d
Himanshu <himanshu32288@gmail.com>
2022-07-24 20:38:35 +0530

    bug

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use("/user/", UsersRoutes);
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
-    app.listen(5000);
+    app.listen(process.env.PORT || 5000);
   })
   .catch((err) => {
     console.log(err);
